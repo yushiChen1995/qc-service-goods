9b1441febba0c7b90c685cb7581f70c2ca4ebffe
chenyushi <chenyushi@lechebang.com>
2019-08-26 16:51:55 +0800

    品牌分页查询

--- src/main/java/com/qc/service/impl/goods/BrandServiceImpl.java
+++ src/main/java/com/qc/service/impl/goods/BrandServiceImpl.java
@@ -1,10 +1,15 @@
 package com.qc.service.impl.goods;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.qc.common.BaseModel;
+import com.qc.common.BaseQueryResult;
+import com.qc.common.PageInfo;
+import com.qc.contract.goods.req.GetBrandRequest;
 import com.qc.dao.goods.BrandMapper;
 import com.qc.example.goods.BrandExample;
 import com.qc.pojo.goods.Brand;
 import com.qc.service.IBrandService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -23,5 +28,30 @@ public class BrandServiceImpl implements IBrandService {
     public List<Brand> getAllBrand() {
         BrandExample example = new BrandExample();
         return brandMapper.selectByExample(example);
+    }
+
+    @Override
+    public BaseQueryResult<Brand> getAllBrandByPage(PageInfo pageInfo) {
+        BrandExample example = new BrandExample();
+        return BaseModel.selectByPage(brandMapper, example, pageInfo);
+    }
+
+    @Override
+    public BaseQueryResult<Brand> getBrandByCondition(GetBrandRequest request) {
+        BrandExample example = new BrandExample();
+        BrandExample.Criteria criteria = example.createCriteria();
+        if (request == null) {
+            return null;
+        }
+        if (request.getSearch() != null) {
+            if (StringUtils.isNotBlank(request.getSearch().getName())) {
+                criteria.andNameLike("%" + request.getSearch().getName() + "%");
+            }
+            if (StringUtils.isNotBlank(request.getSearch().getLetter())) {
+                criteria.andLetterEqualTo(StringUtils.swapCase(request.getSearch().getLetter()));
+            }
+        }
+
+        return BaseModel.selectByPage(brandMapper, example, request.getPage());
     }
 }
